0cdada213db0f2626f0ca7d51a7f60f00afb7a4a
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:17:14 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(array)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   array.sort_by{|word|
     letters=alphabet.split('')
-    sort_val=letters.collect{|char|
+    sort_val=letters.map{|char|
       letters.index(char)}
     sort_val
     
