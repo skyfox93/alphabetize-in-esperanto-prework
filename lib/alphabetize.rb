1bb6b40456b8af3f2122b5135c33a3cab867990f
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:00:59 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
 arry.sort_by{|word|
 letters=word.split('')
 def letter_vals(letter)
-a b c ĉd e f g ĝ h ĥ i j ĵ k l m n o p r s ŝ t u ŭ v z
+abcĉd e f g ĝ h ĥ i j ĵ k l m n o p r s ŝ t u ŭ v z
 
 letters.each{}
 }
