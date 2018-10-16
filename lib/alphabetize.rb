30701310225633fd12e3a90e3f994de8ddddc44d
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:01:41 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,8 @@
 def alphabetize(arr)
-arry.sort_by{|word|
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arry.sort_by{|word|
 letters=word.split('')
 def letter_vals(letter)
-abcĉd e f g ĝ h ĥ i j ĵ k l m n o p r s ŝ t u ŭ v z
 
 letters.each{}
 }
