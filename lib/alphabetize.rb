c73c6fc31e77eb3a43d925502c6df9c3523afd7b
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:42:46 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,11 +2,11 @@ require('pry')
 def alphabetize(array)
   def char_val(char)
     alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-    alphabet.index(char) || 0
+    alphabet.index(char)
   end
 array.sort_by{|word|
   sort_val= word.split('').collect{|char|char_val(char)}
-  sort_val.join.to_i(28) 
+  #sort_val.join.to_i(28) 
     
   }
 
