2bd186f4c57714e4c6005a551a0bbf28882a2a88
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:13:26 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,6 @@ def alphabetize(arr)
   arr.sort_by{|word|
     letters=alphabet.split('')
     word_val=letters.collect{|char|
-      letters.index(char)}
-      word_val.to_i(28)}
+      letters.index(char)}}
 
 end
