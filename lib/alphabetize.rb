7e09c9f35b53e692c7f45029e2d970a3c1332fe0
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 13:55:25 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,19 @@
 def alphabetize(arr)
-arry.each{|word|
-word.split()
+arry.sort_by{|word|
+letters=word.split('')
+def letter_vals(letter_val)
+  case letter val
+a
+b
+c
+ĉ
+d
+e
+f
+g 
+ĝ h ĥ i j ĵ k l m n o p r s ŝ t u ŭ v z
 
+letters.each{}
+}
 
 end
