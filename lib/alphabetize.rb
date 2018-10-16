ecf7381b4c7b49277f1d437efdf00858b32e1bdc
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:23:08 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,13 @@
 require('pry')
 def alphabetize(array)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  def get_index(char)
+  letters=alphabet
+  
+  
   array.sort_by{|word|
     letters=alphabet.split('')
-    sort_val=array.map{|char|
+    sort_val= word.split('').{|char|
     binding.pry
       letters.index(char)}
     sort_val
