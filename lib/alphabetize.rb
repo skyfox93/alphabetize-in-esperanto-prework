da92c596590cfc95eb88a41dc7e1a7051a2a0129
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:03:16 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,6 @@ def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arry.sort_by{|word|
 letters=ESPERANTO_ALPHABET.split('')
-letters.find(word)
+letters.find(word).index
 
 end
