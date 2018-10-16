b460c391c4268afd09850015a1325ca8e0886252
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:41:04 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,8 +6,7 @@ def alphabetize(array)
   end
 array.sort_by{|word|
   sort_val= word.split('').collect{|char|char_val(char)}
-  # sort_val.join.to_i(28) 
-  # doesn't work
+  sort_val.join.to_i(28) 
     
   }
 
