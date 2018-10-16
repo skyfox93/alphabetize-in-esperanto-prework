93c82fa19af55eb51f95d5bb379669ec631a9147
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:10:13 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,8 +2,8 @@ def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arry.sort_by{|word|
 letters=ESPERANTO_ALPHABET.split('')
-word val=0:
-letters.each{
-letters.index(char)
+word_val=letters.collect{|char
+letters.index(char)}
+word_val.to_i(28)
 
 end
