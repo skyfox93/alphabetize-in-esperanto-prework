6cadca45d4509f71a6d71aa66e665ae60cfb2d71
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 14:12:31 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arry.sort_by{|word|
+  arr.sort_by{|word|
     letters=alphabet.split('')
     word_val=letters.collect{|char|
       letters.index(char)}
